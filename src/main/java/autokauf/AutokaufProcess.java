fe013ea31bfb7202575bdf214439bba7d73cc10f
Mike Wieder <mike-wieder@hotmail.de>
2018-04-17 10:52:10 +0200

    init
    init

---
+++ src/main/java/autokauf/AutokaufProcess.java
@@ -0,0 +1,9 @@
+package autokauf;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Autokauf App")
+public class AutokaufProcess extends ServletProcessApplication {
+  // empty implementation
+}
